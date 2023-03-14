1ceb6d859fbebe066f74aa41633d8b8bbc75c52c
paullaster <paullasterokoth98@gmail.com>
2023-03-14 18:06:37 +0300

    add files

--- src/components/GuideCard.vue
+++ src/components/GuideCard.vue
@@ -4,7 +4,7 @@
             <v-card-text>
                 {{ tip.info }}
             </v-card-text>
-            <div class="px-4 d-flex flex-row justify-space-around">
+            <div class="px-4 d-flex flex-row justify-space-between">
                 <v-btn class="d-block">CLOSE</v-btn>
                 <v-btn class="d-block info">NEXT</v-btn>
             </div>
