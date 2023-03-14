edee7f8023d59e81d90e54785655a27b647a1e24
paullaster <paullasterokoth98@gmail.com>
2023-03-14 18:08:48 +0300

    add files

--- src/components/GuideCard.vue
+++ src/components/GuideCard.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container>
-        <v-card v-for="tip in tips" :key="tip.name" dark color="blue" class="mb-2">
+        <v-card v-for="tip in tips" :key="tip.name" dark color="blue" class="pb-4">
             <v-card-text>
                 {{ tip.info }}
             </v-card-text>
@@ -21,7 +21,7 @@ export default {
             tips: [
                 {
                     name: 'home screen',
-                    info: "Let's take tour of the core functions of our application"
+                    info: "Let's take a tour of the core functions of our application"
                 }
             ]
         }
