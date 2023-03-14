668a4743311bcfdb8d64274b608dac4d4e2ba8ba
paullaster <paullasterokoth98@gmail.com>
2023-03-14 18:07:33 +0300

    add files

--- src/components/GuideCard.vue
+++ src/components/GuideCard.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container>
-        <v-card v-for="tip in tips" :key="tip.name" dark color="blue">
+        <v-card v-for="tip in tips" :key="tip.name" dark color="blue" class="mb-2">
             <v-card-text>
                 {{ tip.info }}
             </v-card-text>
