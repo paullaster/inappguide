907fbb735208172ded85a9338467eed831df16fe
paullaster <paullasterokoth98@gmail.com>
2023-03-14 11:56:55 +0300

    add files

--- src/App.vue
+++ src/App.vue
@@ -43,13 +43,13 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    
   },
 
   data: () => ({
